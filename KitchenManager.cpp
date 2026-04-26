0b9c72100d80b34aee07e1faefc7d184c9f1ce48
Usman Hasan <usmanhasan197@gmail.com>
2026-04-26 22:07:47 +0500

    Create KitchenManager.cpp

---
+++ KitchenManager.cpp
@@ -0,0 +1,31 @@
+#include "KitchenManager.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+kitchen_manager::kitchen_manager(order_queue& q)
+    : queue(q), max_stations(3), active_adjustment(0) {}
+
+void kitchen_manager::run() {
+
+    while (running) {
+
+        int qsize = queue.size();
+        int current = max_stations.load();
+
+        if (qsize > 10 && current < 6) {
+            max_stations++;
+            std::cout << "manager: increasing stations to " << max_stations.load() << "\n";
+        }
+        else if (qsize < 3 && current > 2) {
+            max_stations--;
+            std::cout << "manager: decreasing stations to " << max_stations.load() << "\n";
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+int kitchen_manager::get_max_stations() {
+    return max_stations.load();
+}
