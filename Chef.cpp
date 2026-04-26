d0a5ea37eadb2596eb0a60c0fcb04bfcabc10059
Usman Hasan <usmanhasan197@gmail.com>
2026-04-26 22:05:35 +0500

    Create Chef.cpp

---
+++ Chef.cpp
@@ -0,0 +1,27 @@
+#include "Chef.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+chef::chef(order_queue& q, int id) : queue(q), chef_id(id) {}
+
+void chef::run() {
+    while (running) {
+
+        stations.acquire();
+
+        order o = queue.wait_and_pop();
+
+        o.status = order_status::in_progress;
+
+        std::cout << "chef " << chef_id << " started order " << o.order_id << "\n";
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 + (o.order_id % 500)));
+
+        o.status = order_status::done;
+
+        std::cout << "chef " << chef_id << " finished order " << o.order_id << "\n";
+
+        stations.release();
+    }
+}
