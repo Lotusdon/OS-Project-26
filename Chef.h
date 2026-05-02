ff1f47f7516b2264d142d5bd11a444bb7467f187
Usman Hasan <usmanhasan197@gmail.com>
2026-05-02 16:01:31 +0500

    Modify chef constructor to include stats parameter

--- Chef.h
+++ Chef.h
@@ -12,12 +12,13 @@ extern std::counting_semaphore<10> stations;
 class chef {
 private:
     order_queue& queue;
+    stats& system_stats;
     int chef_id;
 
 public:
-    chef(order_queue& q, int id);
+    chef(order_queue& q, stats& s, int id);
 
     void run();
-};
+};;
 
 #endif
