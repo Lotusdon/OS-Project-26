48c0c9bd95be35a6a10f656dfeb3f6f78dba6109
Usman Hasan <usmanhasan197@gmail.com>
2026-05-02 16:00:11 +0500

    Update Waiter.h

--- Waiter.h
+++ Waiter.h
@@ -10,10 +10,11 @@ extern std::atomic<bool> running;
 class waiter {
 private:
     order_queue& queue;
+    stats& system_stats;
     int waiter_id;
 
 public:
-    waiter(order_queue& q, int id);
+    waiter(order_queue& q, stats& s, int id);
 
     void run();
 };
